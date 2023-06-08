7bdcb7b3e4025af642bd6bead299e21a4dec0dfb
Muttam123 <muttamreddy.sh.krishna.tpr@vialto.com>
2023-06-08 10:52:06 +0530

    Thirdcommit

---
+++ src/assets/SubmitPopup.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function SubmitPopup() {
+    const [popup,setPop] = useState(false);
+    const handleClickOpen=()=>{
+      setPop(!popup);
+    } 
+    const handleclosePopup = () => {
+      setPop(!popup);
+  
+    }
+
+  return (
+    <div>
+     <button onClick={handleClickOpen}> Open Popup</button>
+      { popup === true ? (
+           <div className='popup'>
+          <button onClick={handleclosePopup}>X</button>        
+        <img src="https://ik.imagekit.io/otlrg1qg7/Briefing_Questionnaire/Thank_you.jpg?updatedAt=1686117432823" />
+        
+      </div>)
+      : (null)
+
+        }
+    </div>
+  );
+}
+
+export default SubmitPopup;
